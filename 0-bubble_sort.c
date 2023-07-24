a2fe539ce78c8ac3253f082a13f532dbb64815dd
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-07-24 23:54:48 +0100

    upload task 1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, n = size - 1;
+    int tmp;
+
+    while (n > 0)
+    {
+        for (i = 0; i < n; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                tmp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = tmp;
+                print_array(array, size);
+            }
+        }
+        n--;
+    }
+}
--- sort.h
+++ sort.h
@@ -14,22 +14,17 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
-/* Printing functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-/* Advanced sorting algorithms */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -39,4 +34,4 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 
-#endif
+#endif /* _SORT_H_ */
