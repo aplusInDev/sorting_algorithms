54f2b2c75e32519a9bd520ee70c9682a7aa6b2c3
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-07-25 00:07:01 +0000

    last update for task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,21 +10,21 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, n = size - 1;
-    int tmp;
+	size_t i, n = size - 1;
+	int tmp;
 
-    while (n > 0)
-    {
-        for (i = 0; i < n; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                tmp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-        n--;
-    }
+	while (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		n--;
+	}
 }
