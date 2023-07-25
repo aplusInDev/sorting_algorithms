33ab75649bcb21a457cd4df2bac603d929df8fcf
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-07-25 19:28:16 +0100

    fix task 0: check if giving array is not NULL

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,11 +13,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
-	if (size <= 1)
+	if (size <= 1 || array == NULL)
 		return;
 	for (j = 0; j < size - 1; j++)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (i = 0; i < size - j - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
