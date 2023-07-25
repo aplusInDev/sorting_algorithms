e3e90c3143ea01424592efd3fe4eac82396947a9
aplusJsDev <laabid.abdessamadplus@gmail.com>
2023-07-25 12:21:40 +0000

    update all merged tasks: fix betty style

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list -Sorts a doubly linked list of integers in ascending order
- *                        using Insertion sort algorithm
+ * insertion_sort_list - Sorts a doubly linked list of integers in
+ * ascending order using Insertion sort algorithm
  * @list: Double pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
